6d323f905beb2fc6882407cc18673ab6d2c5a8a6
Max Klymyshyn <klymyshyn@gmail.com>
2011-03-31 12:28:44 +0300

    New model method 'chunks' to easy work with chunks in views, new template tag object_chunks_list to get a set of chunks for particular object, support of cache of list of chunks for particular object

--- chunks/models.py
+++ chunks/models.py
@@ -3,9 +3,12 @@ from django.conf import settings
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from django.core.cache import cache
 
 from builders import ChunkBuilder
 
+
+CACHE_PREFIX = 'chunks_obj'
 
 # force init of chunks builders
 CHUNK_BUILDERS_LIST = getattr(settings, 'CHUNK_BUILDERS', [])
@@ -32,6 +35,7 @@ class Chunk(models.Model):
                 return builder.render(request, self, \
                             parent=None, context=context)
         return self.content
+
     def __unicode__(self):
         return u"%s" % (self.key,)
 
@@ -47,18 +51,63 @@ class InlineChunk(models.Model):
     content_object = generic.GenericForeignKey('content_type', 'object_id')
 
     class Meta:
+        ordering = ['order']
         unique_together = ('key', 'content_type', 'object_id')
 
     def __unicode__(self):
         return u"%s / %s" % (self.content_object, self.key)
 
-    def build_content(self, request, context):
+    def build_content(self, request, context, obj=None):
         """Build content with builder or with default builder"""
+
+        if not obj:
+            obj = self.content_object
+
         for builder in CHUNK_BUILDERS:
             if builder.appropriate_key(self.key):
                 return builder.render(request, self, \
-                            parent=self.content_object, context=context)
+                            parent=obj, context=context)
         return self.content
+
+
+class ChunksModel(object):
+    @property
+    def _chunks_cache_key(self):
+        return "%s_%s_%d" % (
+            CACHE_PREFIX,
+            self._meta.object_name.lower(),
+            self.id)
+
+    chunks_cache_key = _chunks_cache_key
+
+    def clear_chunks_cache(self):
+        cache.delete(self.chunks_cache_key)
+
+    def chunks(self, request, context):
+        model_type = ContentType.objects.get_for_model(self.__class__)
+        object_id = self.id
+
+        cache_key = self.chunks_cache_key
+
+        # read cache timeout, if==0 then chage will be disabled
+        cache_timeout = 0
+        if hasattr(self._meta, 'chunks_cache_timeout'):
+            cache_timeout = getattr(self._meta, 'chunks_cache_timeout', 0)
+            if not cache_timeout:
+                cache_timeout = 0
+
+        content = cache.get(cache_key)
+        chunks_content = {}
+        if not content:
+            chunks = InlineChunk.objects.filter(content_type=model_type, \
+                                                    object_id=object_id)
+            # build all chunks for this particular object
+            for ch in chunks:
+                chunks_content[ch.key] = ch.build_content(\
+                                            request, context, obj=self)
+            cache.set(cache_key, chunks_content, cache_timeout)
+        return chunks_content
+
 
 # import and cache all available chunks builders
 for cb in CHUNK_BUILDERS_LIST:
--- chunks/templatetags/chunks.py
+++ chunks/templatetags/chunks.py
@@ -50,6 +50,45 @@ class ObjChunkNode(template.Node):
         return content
 
 
+class ObjChunksListNode(template.Node):
+    """Class to put dictionary with all chunks content into context
+    variable"""
+    def __init__(self, obj, context_name=None):
+        self.obj = template.Variable(obj)
+        quotes = ["'\""]
+        self.context_name = None
+        self.context_name_var = None
+        if context_name[1] in quotes:
+            if context_name[1] != context_name[-1]:
+                raise template.TemplateSyntaxError(\
+                "Context variable name should be quoted "\
+                "with similar quote characters")
+            self.context_name = context_name[1:-1]
+        else:
+            self.context_name_var = template.Variable(context_name)
+
+    def render(self, context):
+        context_name = ""
+        if self.context_name:
+            context_name = self.context_name
+        if self.context_name_var:
+            context_name = self.context_name_var.resolve(context)
+
+        try:
+            obj = self.obj.resolve(context)
+            request = context.get('request', None)
+
+            if not request:
+                raise CONTEXT_IMPROPERLY_CONFIGURED()
+
+            chunks = obj.chunks(request, context)
+
+        except (Chunk.DoesNotExist, template.VariableDoesNotExist):
+            chunks = {}
+        context[context_name] = chunks
+        return ""
+
+
 class ChunkNode(template.Node):
     def __init__(self, key, cache_time=0):
         self.key = key
@@ -96,9 +135,9 @@ def do_get_chunk(parser, token):
 def do_get_object_chunk(parser, token):
     # split_contents() knows not to split quoted strings.
     tokens = token.split_contents()
-    if len(tokens) < 2 or len(tokens) > 3:
+    if len(tokens) < 2 or len(tokens) > 4:
         raise template.TemplateSyntaxError, \
-            "%r tag should have either 2 or 3 arguments" % (tokens[0],)
+            "%r tag should have either 3 or 4 arguments" % (tokens[0],)
     if len(tokens) == 3:
         tag_name, obj, key = tokens
         cache_time = 0
@@ -112,5 +151,18 @@ def do_get_object_chunk(parser, token):
     return ObjChunkNode(obj, key[1:-1], cache_time)
 
 
+def do_get_object_chunks_list(parser, token):
+    # split_contents() knows not to split quoted strings.
+    tokens = token.split_contents()
+    if len(tokens) != 3:
+        raise template.TemplateSyntaxError, \
+            "%r tag should have only 2 arguments" % (tokens[0],)
+
+    tag_name, obj, context_name = tokens
+
+    # Send key without quotes and caching time
+    return ObjChunksListNode(obj, context_name=context_name)
+
 register.tag('chunk', do_get_chunk)
 register.tag('object_chunk', do_get_object_chunk)
+register.tag('object_chunks_list', do_get_object_chunks_list)
