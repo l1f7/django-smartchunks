2200847a9c2fe8fb3492769182f6db2cca72f876
harphphield <harphphield@gmail.com>
2011-10-20 15:36:28 +0200

    comment

--- chunks/middleware.py
+++ chunks/middleware.py
@@ -1,4 +1,8 @@
 class ChunksMiddleware(object):
+    """
+    Monitors all chunks that are created on this request
+    and prints them out in the end.
+    """
     def process_request(self, request):
         self.chunks = {}
         
