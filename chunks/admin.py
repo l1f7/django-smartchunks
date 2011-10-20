4924cfd01bdd90b35c97412c604cd6b3351863d7
harphphield <harphphield@gmail.com>
2011-10-20 12:05:32 +0200

    more description

--- chunks/admin.py
+++ chunks/admin.py
@@ -3,13 +3,13 @@ from models import Chunk, InlineChunk
 
 
 class ChunkAdmin(admin.ModelAdmin):
-    list_display = ('desc', 'key',)
-    search_fields = ('desc', 'key', 'content')
+    list_display = ('description', 'key',)
+    search_fields = ('description', 'key', 'content')
 
 
 class InlineChunkAdmin(admin.ModelAdmin):
     list_display = ('content_object', 'key')
-    search_fields = ('desc', 'key', 'content')
+    search_fields = ('description', 'key', 'content')
 
 
 admin.site.register(Chunk, ChunkAdmin)
