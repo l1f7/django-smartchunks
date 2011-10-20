a636ebcc3139ac0e698fa2165e55d7f2319deb3a
harphphield <harphphield@gmail.com>
2011-10-20 16:04:44 +0200

    a different approach

--- chunks/middleware.py
+++ chunks/middleware.py
@@ -7,6 +7,13 @@ class ChunksMiddleware(object):
         request.generated_chunks = {}
         
     def process_template_response(self, request, response):
-        response.context_data['generated_chunks'] = request.generated_chunks
+        gchunks = []
+        for chunk in request.generated_chunks:
+            gchunks.append({'id': chunk.id,
+                            'key': chunk.key,
+                            'content': chunk.content,
+                            'description': chunk.description})
+            
+        response.context_data['generated_chunks'] = gchunks
         return response
         
