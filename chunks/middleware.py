d0b5d722ba96e8309cd4e1333ba5c9b59b33545e
harphphield <harphphield@gmail.com>
2011-10-20 16:07:17 +0200

    dictionary -> list

--- chunks/middleware.py
+++ chunks/middleware.py
@@ -4,7 +4,7 @@ class ChunksMiddleware(object):
     and prints them out in the end.
     """
     def process_request(self, request):
-        request.generated_chunks = {}
+        request.generated_chunks = []
         
     def process_template_response(self, request, response):
         gchunks = []
