45c13c77510d041763205af2da1e3e881bfe5d5f
harphphield <harphphield@gmail.com>
2011-10-24 17:19:49 +0200

    reverted

--- chunks/models.py
+++ chunks/models.py
@@ -57,7 +57,7 @@ class Chunk(models.Model):
 
 
 class InlineChunk(models.Model):
-    desc = models.CharField(_(u"Description"), max_length=255)
+    description = models.CharField(_(u"Description"), max_length=255)
     key = models.CharField(_(u"Key"), \
                             help_text=_("A name for this chunk of content"), \
                             blank=False, max_length=255)
